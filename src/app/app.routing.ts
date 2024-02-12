41609afac6c9710acd53188e235258c63a377f9b
Akeem McKenzie <akeemmckenzie@hotmail.com>
2024-02-12 01:04:50 -0500

    changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,11 +6,11 @@ import { ThankYouComponent } from './thank-you/thank-you.component';
 import { PreventUnsavedChanges } from './_gaurds/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactComponent, canDeactivate: [PreventUnsavedChanges] },
-  { path: 'thankYou', component: ThankYouComponent },
-  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'angular-portfolio/home', component: HomeComponent },
+  { path: 'angular-portfolio/projects', component: ProjectsComponent },
+  { path: 'angular-portfolio/contact', component: ContactComponent, canDeactivate: [PreventUnsavedChanges] },
+  { path: 'angular-portfolio/thankYou', component: ThankYouComponent },
+  { path: '**', redirectTo: 'angular-portfolio/home', pathMatch: 'full' },
 ];
 
 export const AppRoutes = RouterModule.forChild(routes);
